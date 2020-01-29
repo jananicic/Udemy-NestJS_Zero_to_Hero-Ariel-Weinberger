4a67947ddcb6d4da802ea13400f63c7bc545f27c
Jan Aničić <jansluzbeni@gmail.com>
2020-01-29 17:52:41 +0100

    Add new features for POST, GET and DELETE tasks

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,30 @@
+import {Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
+import {TasksService} from "./tasks.service";
+import {Task} from "./task.model";
+import {CreateTaskDto} from "./dto/create-task.dto";
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private tasksService: TasksService) {
+    }
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
+    }
+
+    @Get("/:id")
+    getTaskById(@Param("id") id: string): Task {
+        return this.tasksService.getTaskById(id);
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+        return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Delete("/:id")
+    deleteTask(@Param("id") id: string): void {
+        return this.tasksService.deleteTask(id);
+    }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@nestjs/common';
+import {Task, TaskStatus} from "./task.model";
+import * as uuid from "uuid/v1";
+import {CreateTaskDto} from "./dto/create-task.dto";
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    getTaskById(id: string) {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const {title, description} = createTaskDto;
+
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.DONE
+        };
+
+        this.tasks.push(task);
+        return task;
+    }
+
+    deleteTask(id: string): void {
+        this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+}
