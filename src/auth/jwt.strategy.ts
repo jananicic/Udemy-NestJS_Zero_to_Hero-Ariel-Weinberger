e78097e1156baea7e53eba9e005bdaab9bc59cf8
Jan Aničić <jansluzbeni@gmail.com>
2020-02-11 09:46:24 +0100

    Add config files for AWS

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,6 +5,7 @@ import {InjectRepository} from "@nestjs/typeorm";
 import {UserRepository} from "./user.repository";
 import {JwtPayload} from "./jwt-payload.inteface";
 import {User} from "./user.entity";
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
                 private userRepository: UserRepository) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: "topSecret51"
+            secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
         });
     }
 
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {Logger} from "@nestjs/common";
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
-  const port = 3000;
+  const serverConfig = config.get('server');
+  const port = process.env.PORT || serverConfig.port;
 
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
